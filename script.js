ee1deb4478b9739aaa6dde1b0c32fc15f58818cb
Lengalei <ruthlengalei@gmail.com>
2024-05-15 13:15:20 +0300

    navbar

--- script.js
+++ script.js
@@ -70,5 +70,5 @@ let navbar = document.querySelector('.navbar');
 
  menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+    navbar.classList.toggle('show');
 }
