dd0b7a08ddbae073cfaa9af4485dfbab2ff375ee
Lengalei <ruthlengalei@gmail.com>
2024-05-20 15:27:53 +0300

    navbar dropdown

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 
     // smooth scrolling
 
-    $('a[href*="#"]').on('click', function(e){
+    $('a[href*=""]').on('click', function(e){
         e.preventDefault();
 
         $('html, body').animate({
@@ -153,3 +153,23 @@ function stopAutoSlide() {
 // Initialize the first testimonial and start the automatic slide
 showTestimonial(currentTestimonial);
 startAutoSlide();
+
+// scrolling reveal
+window.addEventListener('scroll', reveal)
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
